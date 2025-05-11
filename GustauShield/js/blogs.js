9f880d2c1bba9c29aba6d5c3ef39752f04bd8750
Mukhammadali Yuldoshev <133933995+JanGustau17@users.noreply.github.com>
2025-05-11 14:04:27 -0400

    Update blogs.js

--- GustauShield/js/blogs.js
+++ GustauShield/js/blogs.js
@@ -279,7 +279,7 @@ export const ARTICLES = [
   {
     guid: "red-pill-reality",
     title: "Red Pill Reality: Hackers as the Neo of Our Digital World",
-    imageUrl: "assetts/matrix.jpeg",
+    imageUrl: "assets/matrix.jpeg",
     sourceName: "GustauShield Analysis",
     pubDateStr: "2025-05-11T00:00:00Z",
     author: "GustauShield",
